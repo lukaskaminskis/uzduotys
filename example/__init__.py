b71206e5d4da51eb4b6432c22c40fbf23ad09174
Olegasm <oleg.mirzianov@gmail.com>
2020-03-19 19:40:56 +0200

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -1,6 +1,11 @@
 import check50
 
 @check50.check()
-def hello_world():
-    """hello world"""
-    check50.run("python3 hello.py").stdout("Hello there!", regex=False).exit()
+def exists():
+    """Checking if hello.py exists"""
+    check50.exists("hello.py")
+
+@check50.check(exists)
+def prints_hello():
+    """Checking if program prints "Hello there" """
+    check50.run("python3 hello.py").stdout("[Hh]ello, world!?\n", regex=True).exit(0)
